5f20e010aa05db9974d7cd22dedd66f763f79909
Batuhan KATIRCI <katircib@gmail.com>
2023-05-11 13:37:19 +0300

    force cache

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const path = require('path')
-const nocache = require('nocache');
+// const nocache = require('nocache');
 
 const PORT = process.env.PORT || 5001
 
 
 
 express()
-  .use(nocache())
+  .use((req, res, next) => {
+    res.set('Cache-Control', 'public, max-age=31557600, s-maxage=31557600');
+  })
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
