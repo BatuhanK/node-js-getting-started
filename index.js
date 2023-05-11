23404d4f55b23c933a4534d1e55b3f3caa24ad4d
Batuhan KATIRCI <katircib@gmail.com>
2023-05-11 13:40:06 +0300

    lol

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5001
 express()
   .use((req, res, next) => {
     res.set('Cache-Control', 'public, max-age=31557600, s-maxage=31557600');
+    return next()
   })
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
